c20aa10d8d87e4f431268043cfdad220a2d9ecf2
Willian Duarte <willian_xzz@hotmail.com>
2020-01-17 10:06:11 -0300

    Hello World PHP e Modelagem do Banco de dados

---
+++ insertsbanco.sql
@@ -0,0 +1,6 @@
+INSERT INTO `unidb`.`cliente` (`nomecliente`, `cpf`, `email`) VALUES ('antonio', '09209988920', 'antonioalmeida@bol.com.br');
+INSERT INTO `unidb`.`cliente` (`nomecliente`, `cpf`, `email`) VALUES ('juliane', '0920992830', 'julianeferreira@hotmail.com');
+
+
+INSERT INTO `unidb`.`produto` (`nomeproduto`, `valorunitario`, `quantidade`, `codbarra`) VALUES ('Curso de Php', '30,00', '1', '48434874583427');
+INSERT INTO `unidb`.`produto` (`nomeproduto`, `valorunitario`, `quantidade`, `codbarra`) VALUES ('Curso de Javascript', '25,00', '1', '21273327642376432');
